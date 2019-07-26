be34298a28dd3194ba177f738bc407697348b71d
FraserMuir <fraser.muir14@gmail.com>
2019-07-26 13:32:18 +0100

    Test

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,19 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../helpers/ThemeProvider";
-import * as Styled from "../styles/page";
+// import * as Styled from "../styles/page";
 
 export const Page = ({ title, children }) => {
-  const { pageBackground, cardBackground } = useContext(ThemeContext);
+  // const { pageBackground, cardBackground } = useContext(ThemeContext);
 
-  return (
-    <Styled.PageBackground background={pageBackground}>
-      <Styled.PageContent>
-        <Styled.PageTitle>{title}</Styled.PageTitle>
-        <Styled.PageCard background={cardBackground}>
-          {children}
-        </Styled.PageCard>
-      </Styled.PageContent>
-    </Styled.PageBackground>
-  );
+  // return (
+  //   <Styled.PageBackground background={pageBackground}>
+  //     <Styled.PageContent>
+  //       <Styled.PageTitle>{title}</Styled.PageTitle>
+  //       <Styled.PageCard background={cardBackground}>
+  //         {children}
+  //       </Styled.PageCard>
+  //     </Styled.PageContent>
+  //   </Styled.PageBackground>
+  // );
+  return <h1>Hi</h1>;
 };
