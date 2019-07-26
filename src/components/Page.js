496b29db696a070068d7096dbbf520f0412180d7
FraserMuir <fraser.muir14@gmail.com>
2019-07-26 13:39:38 +0100

    Reshuffle page styling

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,19 +1,18 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../helpers/ThemeProvider";
-// import * as Styled from "../styles/page";
+import * as Styled from "./Page.styles";
 
 export const Page = ({ title, children }) => {
-  // const { pageBackground, cardBackground } = useContext(ThemeContext);
+  const { pageBackground, cardBackground } = useContext(ThemeContext);
 
-  // return (
-  //   <Styled.PageBackground background={pageBackground}>
-  //     <Styled.PageContent>
-  //       <Styled.PageTitle>{title}</Styled.PageTitle>
-  //       <Styled.PageCard background={cardBackground}>
-  //         {children}
-  //       </Styled.PageCard>
-  //     </Styled.PageContent>
-  //   </Styled.PageBackground>
-  // );
-  return <h1>Hi</h1>;
+  return (
+    <Styled.PageBackground background={pageBackground}>
+      <Styled.PageContent>
+        <Styled.PageTitle>{title}</Styled.PageTitle>
+        <Styled.PageCard background={cardBackground}>
+          {children}
+        </Styled.PageCard>
+      </Styled.PageContent>
+    </Styled.PageBackground>
+  );
 };
